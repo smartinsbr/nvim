fd9e0b606d7634c2001961715cf9abdfad305503
Samuel Martins <smartinsbr@live.com>
2025-07-06 14:42:19 +0200

    move nvim plugins to individual files

--- init.lua
+++ init.lua
@@ -3,12 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
-}
-
-local opts = {}
+vim.g.mapleader = " "
 
 require("config.lazy")
 
@@ -18,3 +13,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
